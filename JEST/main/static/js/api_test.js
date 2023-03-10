31524e945545d19c15f77f99dc580aaa804978c3
Никита <97441578+JabkaScript@users.noreply.github.com>
2023-03-10 18:28:37 +0300

    fixed db address + why_us request + some design aboba

--- JEST/main/static/js/api_test.js
+++ JEST/main/static/js/api_test.js
@@ -1,16 +1,45 @@
+function create_why_reasons(data){
+  count = data['count'];
+  reasons_data = JSON.parse(data['data']);
+  console.log(reasons_data);
+  reasons_container_html = document.getElementById('#why-block');
+  for(i = 0; i<count; i++){
+    reason = reasons_data[i];
+
+    reason_block_html = document.createElement('div');
+    reason_block_html.className = "why-reason";
+
+    image = document.createElement('img');
+    image.src = reason["image"];
+    image.className = "why-image";
+    reason_block_html.appendChild(image);
+
+    title = document.createElement('h4');
+    title.innerHTML = reason["title"];
+    reason_block_html.appendChild(title);
+
+    text = document.createElement('span');
+    text.innerHTML = reason["text"];
+    text.className = "why-text";
+    reason_block_html.appendChild(text);
+
+    reasons_container_html.appendChild(reason_block_html);
+  };
+}
 
 
 var xhr = new XMLHttpRequest()
 xhr.open(
     'GET',
-    '/faq',
+    '/why-us',
     true,
 )
 
 xhr.onload = (e) => {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
-      console.log(xhr.responseText);
+        data = JSON.parse(xhr.responseText);
+        create_why_reasons(data);
     } else {
       console.error(xhr.statusText);
     }
--- JEST/main/views.py
+++ JEST/main/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from .models import Emploee, About,Faqs
+from .models import Emploee, About, Faqs
 from django.http import JsonResponse
 import json
 
 
-def render_main (request):
+def render_main(request):
     return render(request, 'main/index.html')
+
 
 def api_test(request):
     name_for_filter = request.GET.get('name')
     data = []
     for el in Emploee.objects.raw(
-        "select * from main_emploee\n"
-        f"where name = '{name_for_filter}'"):
+            "select * from main_emploee\n"
+            f"where name = '{name_for_filter}'"):
         data.append(el)
     return JsonResponse(
         {
@@ -20,44 +21,37 @@ def api_test(request):
         }
     )
 
+
 def why_us(request):
-    block_data = {
-        'title':'',
-        'text':'',
-        'image':''
-            }
     data = []
-    for el in AboutUs.objects.all():
-        block_data = {'title':el.title,
-                'text': el.text,
-                'image':el.image
-                }
-        data.append(json)
+    for el in About.objects.all():
+        block_data = {'title': el.title,
+                      'text': el.text,
+                      'image': el.image
+                      }
+        data.append(block_data)
 
     return JsonResponse(
         {
             'count': f'{len(data)}',
-           'data': f"{json.dumps(data)}"
+            'data': f"{json.dumps(data)}"
         }
     )
 
+
 def faq(request):
-    block_data = {
-        'question':'',
-        'answer':''
-            }
     data = []
     for el in Faqs.objects.all():
-        block_data = {'question':el.question,
-                'answer': el.answer,
-                }
+        block_data = {'question': el.question,
+                      'answer': el.answer,
+                      }
         data.append(block_data)
-    print ()
+    print()
 
     return JsonResponse(
         {
             'count': f'{len(data)}',
-           'data': f"{json.dumps(data)}"
+            'data': f"{json.dumps(data)}"
         }
     )
 
