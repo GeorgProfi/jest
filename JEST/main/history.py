2da980b0de6be7bc1068e2c3b2748f73af82e37b
Huawei <poroshin.34z@gmail.com>
2023-03-12 13:13:57 +0300

    запрос для страницы История

--- JEST/main/history.py
+++ JEST/main/history.py
@@ -4,11 +4,12 @@ from django.http import JsonResponse
 import json
 from django.db import connection
 import datetime
+from .models import History
 
 
 def history(request):
     data = []
-    for el in AboutUs.objects.all():
+    for el in History.objects.all():
         block_data = {'title': el.title,
                       'text': el.text,
                       'image': el.image
