18de14cc9d9de2298d05cbc1362f7537c1ae5bcb
Никита <97441578+JabkaScript@users.noreply.github.com>
2023-03-12 14:59:19 +0300

    added faqs + code refactoring (navbar+footer ya debik lol)

--- JEST/main/static/js/index.js
+++ JEST/main/static/js/index.js
@@ -19,7 +19,7 @@ async function create_reviews(){
   data = await createAsyncGETRequest('reviews?count=5')
   count = data['count'];
   reviews_data = JSON.parse(data['data']);
-  reviews_container_html = document.getElementById('#reviews-block');
+  reviews_container_html = document.getElementById('reviews-block');
   for(i = 0; i<count; i++){
     review = reviews_data[i];
 
@@ -50,7 +50,7 @@ async function create_why_reasons(){
   data = await createAsyncGETRequest('why-us/')
   count = data['count'];
   reasons_data = JSON.parse(data['data']);
-  reasons_container_html = document.getElementById('#why-block');
+  reasons_container_html = document.getElementById('why-block');
   for(i = 0; i<count; i++){
     reason = reasons_data[i];
 
@@ -75,6 +75,51 @@ async function create_why_reasons(){
   };
 }
 
+async function create_faq(){
+  data = await createAsyncGETRequest('faq/')
+  count = data['count'];
+  faqs_data = JSON.parse(data['data']);
+  faqs_block_html = document.getElementById('faq-block');
+  for(i = 0; i<count; i++){
+    faq = faqs_data[i];
+
+    faq_container = document.createElement('a');
+    faq_container.className = "faq-container";
+    faq_container.setAttribute('onclick',"open_faq(this)");
+
+    question = document.createElement('span');
+    question.className = "faq-question";
+    question.innerHTML = faq['question'];
+    faq_container.appendChild(question);
+
+    div_line = document.createElement('div');
+    div_line.className = "faq-line";
+    faq_container.appendChild(div_line);
+
+    answer = document.createElement('span');
+    answer.className = "faq-answer";
+    answer.innerHTML = faq['answer'];
+    faq_container.appendChild(answer);
+
+    faqs_block_html.appendChild(faq_container);
+  };
+}
+
+async function open_faq(elem){
+  elem.setAttribute('onclick',"close_faq(this)");
+  for(let i = 1; i<elem.children.length; i++){
+   elem.children[i].style='display:inline';
+  }
+}
+
+async function close_faq(elem){
+  elem.setAttribute('onclick',"open_faq(this)");
+  for(let i = 1; i<elem.children.length; i++){
+   elem.children[i].style='display:none';
+  }
+}
+
 create_why_reasons();
 create_reviews();
+create_faq();
 
