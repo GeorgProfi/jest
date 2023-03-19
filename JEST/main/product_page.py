347c8305a01c4dcfacd30b10ef656a6dbcbfef3b
Huawei <poroshin.34z@gmail.com>
2023-03-19 21:22:24 +0300

    страница товара

---
+++ JEST/main/product_page.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.db import connection
+from . import dictfetchall as df
+import json
+
+
+def render_product_page(request, product_id):
+    return render(request, 'main/index.html') #изменить шаблон
+
+
+def product_info(request):
+    product_id = request.GET['id']
+    with connection.cursor() as cursor:
+        cursor.execute(f'select * from products where id = {product_id}')
+        row = df.dictfetchall(cursor)[0]
+
+        data = {
+            'id'          : row['id'],
+            'title'       : row['title'],
+            'description' : row['description'],
+            'price'       : row['price'],
+            'images': {
+                'image_1': str(json.loads(row['photos'])['img1']),
+                'image_2': str(json.loads(row['photos'])['img2']),
+                'image_3': str(json.loads(row['photos'])['img3'])
+            },
+            'material'    : row['metals'],
+            'size'        : row['size'],
+            'gems'        : row['gems'],
+            'mass'        : row['weight']
+        }
+    return JsonResponse(
+        data
+    )
