5d0ac3b55b7823eeb7132c42f876eacda4e9a073
Никита <97441578+JabkaScript@users.noreply.github.com>
2023-03-20 22:33:06 +0300

    some catalog style changes

--- JEST/main/static/js/catalog.js
+++ JEST/main/static/js/catalog.js
@@ -1,3 +1,17 @@
+async function createAsyncGETRequest(url){
+    return new Promise((resolve, reject) => {
+      fetch(url).then(response=>{
+        if(response.status==200){
+          data = response.json();
+          return resolve(data);
+        } else {
+          return reject(response.status);
+        }
+      }
+      )
+      });
+}
+
 function showFilterContent(elem){
     box = elem.parentNode.parentNode;
     elem.className="arrow-btn-u"
@@ -79,3 +93,90 @@ function priceInput(){
     setSliderPadding((min_price.value/min_price.max)*100, (1-max_price.value/max_price.max)*100);
     setRangeValue(min_price.value, max_price.value);
 }
+
+async function create_products(){
+    data = await createAsyncGETRequest('/products?count=10&already_in_page=0');
+    count = data['count'];
+    products_data = JSON.parse(data['data']);
+    html_grid = document.getElementById('products-grid');
+    html_count = document.getElementById('products-count');
+    for(i = 0; i<count; i++){
+        product = products_data[i];
+    
+        card = document.createElement('a')
+        card.className = 'product-card flex-column';
+        card.setAttribute('onMouseOver',"this.children[1].style=''");
+        card.setAttribute('onMouseOut',"this.children[1].style='display:none;'");
+
+        visible_part = document.createElement('div');
+        visible_part.className = 'card-visible-part flex-column';
+        card.appendChild(visible_part);
+
+        image = document.createElement('img');
+        image.className = 'product-image centered';
+        image.src = product['image'];
+        visible_part.appendChild(image);
+
+        product_visible_info = document.createElement('div');
+        product_visible_info.className = 'product-info flex-column';
+        
+        title = document.createElement('span');
+        title.className = 'product-big-text';
+        title.innerHTML = product['title'];
+        product_visible_info.appendChild(title);
+
+        price = document.createElement('span');
+        price.className = 'product-big-text';
+        price.innerHTML = product['price'] + '₽';
+        product_visible_info.appendChild(price);
+
+        visible_part.appendChild(product_visible_info);
+        card.append(visible_part);
+
+        hover_part = document.createElement('div');
+        hover_part.className = 'card-hover-part flex-column';
+        hover_part.style='display:none;';
+
+        cart_button = document.createElement('button');
+        cart_button.className = 'focus-btn cart-btn';
+        cart_button.innerHTML = 'В корзину';
+        hover_part.appendChild(cart_button);
+
+        add_info = document.createElement('div');
+        add_info.className = 'product-add-info flex-row';
+
+        materials_headers = document.createElement('div')
+        materials_headers.className = 'product-add-info-col flex-column';
+        headers_m = ['Металл', 'Проба'];
+
+        materials_values = document.createElement('div')
+        materials_values.className = 'product-add-info-col flex-column';
+        values = ['title', 'probe'];
+
+        materials = product['material'];
+        
+        for(j = 0; j<materials.length; j++){
+            material = materials[j];
+            for(z = 0; z<headers_m.length; z++){
+                header = document.createElement('span');
+                header.className = 'product-small-text';
+                header.innerHTML = headers_m[z];
+                materials_headers.appendChild(header);
+                header_value = document.createElement('span');
+                header_value.className = 'product-small-text fg-color';
+                header_value.innerHTML = material[values[z]];
+                materials_values.appendChild(header_value);
+            }
+
+        }
+        add_info.appendChild(materials_headers);
+        add_info.appendChild(materials_values);
+
+        hover_part.appendChild(add_info);
+        card.append(hover_part);
+        html_grid.appendChild(card);
+    }
+    html_count.innerHTML = `Результат: ${html_grid.children.length}`;
+}
+
+create_products();
