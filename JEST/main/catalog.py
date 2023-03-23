098e84ab236ed5ee39ea6bda029306513914690e
Huawei <poroshin.34z@gmail.com>
2023-03-23 09:45:27 +0300

    Исправлены пакости с моделями x2

--- JEST/main/catalog.py
+++ JEST/main/catalog.py
@@ -71,42 +71,34 @@ def products(request):
         query += ')'
 
 
-
-
-
-
-
-
-
-
     if 'title' in url_parameters:
-    title = request.GET['title']
-    query += f" and products.title = '{title}'"
+        title = request.GET['title']
+        query += f" and products.title = '{title}'"
 
-    query += f"""
-    order by products.id
-    limit {count}
-    offset {already_in_page};
-    """
-    print(query)
+        query += f"""
+        order by products.id
+        limit {count}
+        offset {already_in_page};
+        """
+        print(query)
 
     with connection.cursor() as cursor:
-    data = []
-    cursor.execute(query)
-    rows = df.dictfetchall(cursor)
-    print(rows)
-    for row in rows:
-        block_data = {
-            'id': row['id'],
-            'title': row['title'],
-            'price': row['price'],
-            'image': str(json.loads(row['photos'])['img1']),
-            'material': row['metals'],
-            'size': row['size'],
-            'gems': row['gems'],
-            'mass': row['weight'],
-        }
-        data.append(block_data)
+        data = []
+        cursor.execute(query)
+        rows = df.dictfetchall(cursor)
+        print(rows)
+        for row in rows:
+            block_data = {
+                'id': row['id'],
+                'title': row['title'],
+                'price': row['price'],
+                'image': str(json.loads(row['photos'])['img1']),
+                'material': row['metals'],
+                'size': row['size'],
+                'gems': row['gems'],
+                'mass': row['weight'],
+            }
+            data.append(block_data)
     return JsonResponse(
         {
             'count': f"{len(data)}",
