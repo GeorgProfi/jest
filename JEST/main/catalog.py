4d670a522a2aca5190d03fa9fc20236e73d95247
Huawei <poroshin.34z@gmail.com>
2023-03-23 09:42:34 +0300

    Исправлены пакости

--- JEST/main/catalog.py
+++ JEST/main/catalog.py
@@ -15,33 +15,41 @@ def products(request):
     url_parameters = list(request.GET.keys())
     count = request.GET['count']
     already_in_page = request.GET['already_in_page']
-    query = f"""
+    query = """
         select distinct on (id)
         id, title, price, photos, size, weight, description, category, collection, gems, metals from products
-        left join lateral jsonb_array_elements ( to_jsonb(metals) ) filter1 ON true
-        left join lateral jsonb_array_elements ( to_jsonb(gems) ) filter2 ON true
-        where products.id is not null
-    """
+        cross join lateral jsonb_array_elements (
+        case
+        when to_jsonb(metals) is null then '[{}]'
+        else to_jsonb(metals) end
+        ) as filter1
+        cross join lateral jsonb_array_elements (
+        case
+        when to_jsonb(gems) is null then '[{}]'
+        else to_jsonb(gems) end
+        ) as filter2
+        where products.id is not null"""
     if 'probes' in url_parameters:
         probe = request.GET['probes'].split()
-        query += f" and (filter1->>'probe' = '{probe[0]}'"
+        query += f" and (filter1-»'probe' = '{probe[0]}'"
         for i in range(1, len(probe)):
-            query += f" or filter1->>'probe' ='{probe[i]}'"
+            query += f" or filter1-»'probe' ='{probe[i]}'"
         query += ')'
 
     if 'probes_and_metals' in url_parameters:
         metals = request.GET['probes_and_metals'].split()
-        query += f" and (filter1->>'title' = '{metals[0]}'"
+        query += f" and (filter1-»'title' = '{metals[0]}'"
         for i in range(1, len(metals)):
-            query += f" or filter1->>'title' ='{metals[i]}'"
+            query += f" or filter1-»'title' ='{metals[i]}'"
         query += ')'
 
     if 'gems' in url_parameters:
         gems = request.GET['gems'].split()
-        query += f" and (filter2->>'title' = '{gems[0]}'"
+        query += f" and (filter2-»'title' = '{gems[0]}'"
         for i in range(1, len(gems)):
-            query += f" or filter2->>'title' ='{gems[i]}'"
+            query += f" or filter2-»'title' ='{gems[i]}'"
         query += ')'
+
 
     if 'sizes_and_categories' in url_parameters:
         category = request.GET['sizes_and_categories'].split()
@@ -62,35 +70,43 @@ def products(request):
             query += f" or products.collection = '{collection[i]}'"
         query += ')'
 
+
+
+
+
+
+
+
+
+
     if 'title' in url_parameters:
-        title = request.GET['title']
-        query += f" and products.title = '{title}'"
+    title = request.GET['title']
+    query += f" and products.title = '{title}'"
 
     query += f"""
-        order by products.id
-        limit {count} 
+    order by products.id
+    limit {count}
+    offset {already_in_page};
     """
-    if int(already_in_page) > 0:
-        query += f"""offset {already_in_page} rows """
     print(query)
 
     with connection.cursor() as cursor:
-        data = []
-        cursor.execute(query)
-        rows = df.dictfetchall(cursor)
-        print(rows)
-        for row in rows:
-            block_data = {
-                'id': row['id'],
-                'title': row['title'],
-                'price': row['price'],
-                'image': str(json.loads(row['photos'])['img1']),
-                'material': row['metals'],
-                'size': row['size'],
-                'gems': row['gems'],
-                'mass': row['weight'],
-            }
-            data.append(block_data)
+    data = []
+    cursor.execute(query)
+    rows = df.dictfetchall(cursor)
+    print(rows)
+    for row in rows:
+        block_data = {
+            'id': row['id'],
+            'title': row['title'],
+            'price': row['price'],
+            'image': str(json.loads(row['photos'])['img1']),
+            'material': row['metals'],
+            'size': row['size'],
+            'gems': row['gems'],
+            'mass': row['weight'],
+        }
+        data.append(block_data)
     return JsonResponse(
         {
             'count': f"{len(data)}",
@@ -102,9 +118,10 @@ def products(request):
 def categories(request):
     data = []
     for el in Category.objects.all():
-        block_data = {'id': el.id,
-                      'title': el.title,
-                      }
+        block_data = {
+            'id': el.id,
+            'title': el.title,
+        }
         data.append(block_data)
     return JsonResponse(
         {
@@ -118,15 +135,15 @@ def filters(request):
     with connection.cursor() as cursor:
         cursor.execute('select * from filters;')
         data = df.dictfetchall(cursor)[0]
-    return JsonResponse(
-        {
-            'count': f'{len(data)}',
-            'data': {
-                'sizes_and_categories': data['sizes_and_categories'],
-                'collections': data['collections'],
-                'probes_and_metals': data['probes_and_metals'],
-                'gems': data['gems'],
-                'max_price': data['max_price']
+        return JsonResponse(
+            {
+                'count': f'{len(data)}',
+                'data': {
+                    'sizes_and_categories': data['sizes_and_categories'],
+                    'collections': data['collections'],
+                    'probes_and_metals': data['probes_and_metals'],
+                    'gems': data['gems'],
+                    'max_price': data['max_price']
+                }
             }
-        }
-    )
+        )
