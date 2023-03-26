5867ecfba92d7ab766d5b833877ef3776260ae68
GeorgProfi <georgprofi04@gmail.com>
2023-03-26 18:48:08 +0300

    add csrf token

--- JEST/main/sessionlogic.py
+++ JEST/main/sessionlogic.py
@@ -1,7 +1,12 @@
 import uuid
 
+import django
+
+
 def gen_session(request):
     session = request.session
     UUID = session.get('UUID')
+    django.middleware.csrf.get_token(request)
+    print(request.headers)
     if UUID is None:
         session['UUID'] = str(uuid.uuid4())
