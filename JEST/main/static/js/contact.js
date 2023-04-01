059fe0628534c81bbd26f652e2c7467a63d34287
feliis <99050400+feliis@users.noreply.github.com>
2023-04-01 23:07:15 +0300

    contacts script + fixed history carousel

--- JEST/main/static/js/contact.js
+++ JEST/main/static/js/contact.js
@@ -14,6 +14,43 @@ async function createAsyncGETRequest(url){
 
   async function createMasters(){
     data = await createAsyncGETRequest('/masters');
-    console.log(data);
+    count = data['count'];
+    masters_data = JSON.parse(data['data']);
+    masters_container_html = document.querySelector('.emploee-cards');
+    masters_container_html.innerHTML = '';
+    for(i = 0; i<count; i++){
+      master = masters_data[i];
+  
+      console.log(master);
+      card_container_html = document.createElement('div');
+      card_container_html.className = "card flex-row";
+  
+      info_block_html = document.createElement('div');
+      info_block_html.className = "emploee-info flex-column";
+  
+      image = document.createElement('img');
+      image.src = master["image"];
+      image.className = "profile-image centered";
+      card_container_html.appendChild(image);
+  
+      emploee_name = document.createElement('h4');
+      emploee_name.className = "name";
+      emploee_name.innerHTML = master["name"] + "\n" + master["surname"];
+      emploee_name.style="white-space:line;"
+      info_block_html.appendChild(emploee_name);
+  
+      post = document.createElement('h4');
+      post.innerHTML = master["post"];
+      post.className = "post";
+      info_block_html.appendChild(post);
+  
+      text = document.createElement('span');
+      text.innerHTML = master["text"];
+      text.className = "discription";
+      info_block_html.appendChild(text);
+  
+      card_container_html.appendChild(info_block_html);
+      masters_container_html.appendChild(card_container_html);
+    }
   }
   createMasters();
